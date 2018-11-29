69879cb3fbf78b23932d25d17d389427b8865e0f
Pannapur <pannapur@in-Pannapur.local>
2018-11-29 17:34:23 +0530

    Fixed indentation issues in test file.

--- test/gameOfLifeTest.js
+++ test/gameOfLifeTest.js
@@ -29,7 +29,8 @@ describe('nextGeneration',() => {
     assert.ok(isSame(actualNextGen,expectedNextGen));
     assert.ok(isSameArity(actualNextGen,expectedNextGen));
   });
-    it("should return an verical blinker when given horizontal blinker with negative coordinates", () => {
+
+  it("should return an verical blinker when given horizontal blinker with negative coordinates", () => {
     let currentGeneration = [[0, 0], [0, 1], [0, 2]];
     let expectedNextGen = [[-1,1], [0,1], [1, 1]];
     let bounds = {topLeft: [-1, -1], bottomRight: [2, 2]};
@@ -37,6 +38,7 @@ describe('nextGeneration',() => {
     assert.ok(isSame(actualNextGen, expectedNextGen));
     assert.ok(isSameArity(actualNextGen, expectedNextGen));
   });
+
   it("should return an empty generation for a current generation that contains only one live cell", () => {
     let currentGeneration = [[-3,-1],[-2,-5],[1,0],[-1,-1]];
     let expectedNextGen = [];
@@ -45,6 +47,7 @@ describe('nextGeneration',() => {
     assert.ok(isSame(actualNextGen, expectedNextGen));
     assert.ok(isSameArity(actualNextGen, expectedNextGen));
   });
+
   it("should return a generation with two horizontal alive cells", () => {
     let currentGeneration = [[0,2],[0, 3],[2,2]];
     let expectedNextGen = [[1,2],[1,3]];
@@ -53,6 +56,7 @@ describe('nextGeneration',() => {
     assert.ok(isSame(actualNextGen, expectedNextGen));
     assert.ok(isSameArity(actualNextGen, expectedNextGen));
   });
+
   it('should return the same generation',() => {
     let currentGeneration = [[1,2],[2,1],[2,3],[3,2]];
     let expectedNextGen = [[1,2],[2,1],[2,3],[3,2]]
@@ -61,6 +65,7 @@ describe('nextGeneration',() => {
     assert.ok(isSame(actualNextGen,expectedNextGen));
     assert.ok(isSameArity(actualNextGen,expectedNextGen));
   });
+
   it('should return the same generation',() => {
     let currentGeneration = [[2,3],[2,4],[3,2],[3,5],[4,3],[4,4]];
     let expectedNextGen = [[2,3],[2,4],[3,2],[3,5],[4,3],[4,4]];
@@ -71,8 +76,8 @@ describe('nextGeneration',() => {
   });
 });
 
-describe("new test " ,function(){
+describe("should return generation " ,function(){
   it("behive",function(){
     assert.deepEqual(nextGeneration([[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]],{topLeft:[-10,-3],bottomRight:[4,5]}),[[1,2],[1,3],[2,1],[2,4],[3,2],[3,3]]);});
-  
+
 });
