955853152c779640ea8f8acd19ef95febc5f7da9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-01 03:29:18 +0000

    Deploy STEP-tw/gol-anupingale to github.com/STEP-tw/gol-anupingale.git:gh-pages

---
+++ precache-manifest.be0f6f60f5761c6fad7976e3aa9dc99e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "949cd988786b56dad461",
+    "url": "/gol-anupingale/static/js/runtime~main.949cd988.js"
+  },
+  {
+    "revision": "3124e9312818c37287ad",
+    "url": "/gol-anupingale/static/js/main.3124e931.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/gol-anupingale/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "3124e9312818c37287ad",
+    "url": "/gol-anupingale/static/css/main.eb0a4a5a.chunk.css"
+  },
+  {
+    "revision": "218ef2bbaf9375f300568eb69e5d1a84",
+    "url": "/gol-anupingale/index.html"
+  }
+];
